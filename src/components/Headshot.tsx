ebe284cff408744fa31d39432c11631bfd776494
Cody Payne <copayne@gmail.com>
2023-11-12 00:28:37 -0500

    resolve linter errors

--- src/components/Headshot.tsx
+++ src/components/Headshot.tsx
@@ -1,22 +1,26 @@
 import Image from 'next/image';
-import { HEADSHOTS, type HeadshotData } from '../helpers/getHeadshots';
+import { HEADSHOTS } from '../helpers/getHeadshots';
 
 interface Headshot {
   actor: string,
 }
 
 const Headshot = ({ actor }: Headshot) => {
-  const headshot: HeadshotData = HEADSHOTS[actor];
+  const headshot = HEADSHOTS.find(h => h.id === actor);
 
   return (
     <div className="p-2 w-full flex flex-col items-center">
       <div className="border-2 border-black w-[75px] h-[75px]">
-        <Image
-          alt={`${actor} headshot`}
-          src={headshot.src}
-          height="75"
-          width="75"
-        />
+        {
+          headshot && (
+            <Image
+              alt={`${actor} headshot`}
+              src={headshot.src}
+              height="75"
+              width="75"
+            />
+          )
+        }
       </div>
       <p className="capitalize underline underline-offset-4 text-dark-secondary text-sm text-center">
         {actor}
