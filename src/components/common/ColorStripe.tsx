e6c14b7d0341b46bbc1f463c927f2df921f7a734
Cody Payne <copayne@gmail.com>
2023-11-26 21:01:38 -0500

    fix dynamic color classes in color stripe

--- src/components/common/ColorStripe.tsx
+++ src/components/common/ColorStripe.tsx
@@ -1,10 +1,19 @@
+import { clsx } from 'clsx';
+
 interface ColorStripe {
   text?: string,
   color: string,
 }
 
 const ColorStripe = ({ text = '', color }: ColorStripe) => (
-  <div className={`w-full bg-default-${color} text-lg text-default-white font-semibold h-full`}>
+  <div className={clsx(
+    'w-full text-lg text-default-white font-semibold h-full',
+    color === 'purple' && 'bg-default-purple',
+    color === 'pink' && 'bg-default-pink',
+    color === 'red' && 'bg-default-red',
+    color === 'orange' && 'bg-default-orange',
+
+  )}>
     {
       text && (
         <span className="pl-2">{text.toLocaleUpperCase()}</span>
